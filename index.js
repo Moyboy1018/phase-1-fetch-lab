2ecc37ddaa6ab26795fca0c19aa7f306edd39227
Moyboy <Vincent.moy94@gmail.com>
2024-08-26 22:36:17 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      renderBooks(data);
+    })
 }
 
 function renderBooks(books) {
@@ -14,4 +20,4 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+})
